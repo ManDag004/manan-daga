52eaa57b774ba6059f49ad014c772354220c22b2
Manan Daga <manandaga2004@gmail.com>
2023-10-30 17:03:42 -0700

    Create resusable menu

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
 document.addEventListener('DOMContentLoaded', function() {
+  const body = document.querySelector('body');
+  const menuTemplate = document.createElement('header');
+  
+  menuTemplate.innerHTML = `
+  <div id="menuIcon" class="menu-icon">
+  <i class="fas fa-bars"></i>
+  </div>
+  
+  <div id="menu">
+  <div id="menu-item-container">
+  <a class="menu-item" href="index.html">About</a>
+  <a class="menu-item" href="projects.html">Projects</a>
+  <a class="menu-item" href="resume.html">Resume</a>
+  <a class="menu-item" href="transcript.html">Transcript</a>
+  </div>
+  <div id="menu-background"></div>
+  </div>
+  `;
+  
+  body.insertBefore(menuTemplate, body.firstChild);
+  
+  
   const menuIcon = document.getElementById('menuIcon');
   const sections = document.getElementsByTagName('section');
   const menu = document.getElementById('menu');
   let clicked = false;
-
+  
   function menuClick() {
+    console.log('clicked');
     clicked = !clicked;
     
     if (clicked === true) {
