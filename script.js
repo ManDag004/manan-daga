9d302fda5d060b3ec61ee2e9082b35cfbf6eaf55
Manan Daga <manandaga2004@gmail.com>
2023-11-05 14:13:44 -0800

    Add Dark Mode oggle Aniamtions

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
   <div id="menuIcon" class="menu-icon">
   <i class="fas fa-bars"></i>
   </div>
-
+  
   <div class="dark-mode-icon">
   <i class="fas fa-sun"></i>
   </div>
@@ -32,6 +32,7 @@ document.addEventListener('DOMContentLoaded', function() {
   const menuBackground = document.getElementById('menu-background');
   const darkModeIcon = document.querySelector('.dark-mode-icon');
   let clicked = false;
+  setDarkMode();
   
   function menuClick() {
     clicked = !clicked;
@@ -61,32 +62,82 @@ document.addEventListener('DOMContentLoaded', function() {
       .set(".menu-item", {clearProps: "all"});
     }
   }
-  
-  menuIcon.addEventListener('click', menuClick);
 
-  darkModeIcon.addEventListener('click', () => {
+  function setDarkMode() {
+    if (localStorage.getItem('darkMode') === 'true') {
+      gsap.to(document.documentElement, {
+        '--background-color': '#333',
+        '--text-color': '#fff',
+        '--color-num': 255,
+        duration: 0,
+      });
+      darkModeIcon.innerHTML = '<i class="fas fa-sun"></i>';
+    } else {
+      gsap.to(document.documentElement, {
+        '--background-color': '#fff',
+        '--text-color': '#333',
+        '--color-num': 0,
+        duration: 0,
+      });
+      darkModeIcon.innerHTML = '<i class="fas fa-moon"></i>';
+    }
+  }
+  
+  function darkModeToggle() {
+    console.log('clicked');
+    console.log('is it dark mode:', localStorage.getItem('darkMode'));
     if (localStorage.getItem('darkMode') === 'true') {
       localStorage.setItem('darkMode', 'false');
-      darkModeIcon.innerHTML = '<i class="fas fa-sun"></i>';
-      // change css variables
-      document.documentElement.style.setProperty('--background-color', '#333');
-      document.documentElement.style.setProperty('--text-color', '#fff');
-      document.documentElement.style.setProperty('--color-num', 255);
       
+      gsap.to(darkModeIcon, {
+        rotate: 180, 
+        scale: 0, 
+        opacity: 0.5, 
+        duration: 0.5,
+        onComplete: () => {
+          darkModeIcon.innerHTML = '<i class="fas fa-moon"></i>';
+          gsap.fromTo(darkModeIcon, { scale: 0, opacity: 0, rotate: -180 }, { scale: 1, opacity: 1, duration: 0.5, rotate: 0 });
+        },
+      });
+      
+      gsap.to(document.documentElement, {
+        '--background-color': '#fff',
+        '--text-color': '#333',
+        '--color-num': 0,
+        duration: 0.5,
+      });
     } else {
       localStorage.setItem('darkMode', 'true');
-      darkModeIcon.innerHTML = '<i class="fas fa-moon"></i>';
-      document.documentElement.style.setProperty('--background-color', '#fff');
-      document.documentElement.style.setProperty('--text-color', '#333');
-      document.documentElement.style.setProperty('--color-num', 0);
+      
+      gsap.to(darkModeIcon, {
+        rotate: -180,
+        scale: 0,
+        opacity: 0.5,
+        duration: 0.5,
+        onComplete: () => {
+          darkModeIcon.innerHTML = '<i class="fas fa-sun"></i>';
+          gsap.fromTo(darkModeIcon, { scale: 0.8, opacity: 0, rotate: 180 }, { scale: 1, opacity: 1, duration: 0.5, rotate: 0 });
+        },
+      });
+      
+      gsap.to(document.documentElement, {
+        '--background-color': '#333',
+        '--text-color': '#fff',
+        '--color-num': 255,
+        duration: 0.5,
+      });
     }
-  })
-
-
+  }
+  
+  menuIcon.addEventListener('click', menuClick);
+  
+  darkModeIcon.addEventListener('click', darkModeToggle)
+  
+  
   menuItemsContainer.addEventListener('mouseover', () => {
     gsap.to(menuBackground, {duration: 0.25, opacity: 0.3, backgroundSize: "9vh 9vh"});
   });
-
+  
   menuItemsContainer.addEventListener('mouseout', () => {
     gsap.to(menuBackground, {duration: 0.25, opacity: 1, backgroundSize: "10vh 10vh"});
   });
@@ -95,7 +146,7 @@ document.addEventListener('DOMContentLoaded', function() {
   .forEach((item, index) => {
     item.onmouseover = () => {
       menu.dataset.activeIndex = index;
-
+      
       gsap.to(menuBackground, {
         backgroundPosition: `100% -${25 * (index+1)}%`,
         duration: 0.5, // Adjust the duration as needed
